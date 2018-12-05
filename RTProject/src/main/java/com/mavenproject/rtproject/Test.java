f07bd5053c3cf60bd7c297d0ffe359b62c101135
Gifhary Syidhqa Hamim <43630682+gifhary@users.noreply.github.com>
2018-12-05 23:17:13 +0800

    Update Test.java

--- RTProject/src/main/java/com/mavenproject/rtproject/Test.java
+++ RTProject/src/main/java/com/mavenproject/rtproject/Test.java
@@ -16,18 +16,17 @@ public class Test {
 
     public static void main(String[] args) throws MalformedURLException {
 
-        String sUrl = "https://github.com/gifhary/Test_File/archive/master.zip";
-        URL url = new URL(sUrl);
+        URL url = new URL("https://github.com/gifhary/Test_File/archive/master.zip");//github repo download link
 
-        File setFileName = new File("TargetFile.zip");
-        String zipFile = "TargetFile.zip";
+        File setFileName = new File("TargetFile.zip");//set the file downloaded file name
+        String zipFile = "TargetFile.zip";//the file name in String
 
-        AccessFiles m = new AccessFiles();
-        m.downloadZip(url, setFileName);
-        m.unzip(zipFile);
+        AccessFiles m = new AccessFiles();//declare accessFile object
+        m.downloadZip(url, setFileName);//downloading file from URL and the file name
+        m.unzip(zipFile);//extract zip file, it cal also extract any other zip file
 
-        ArrayList<String> list = new ArrayList();
-        list = m.readFiles(m.listFiles());
+        ArrayList<String> list = new ArrayList();//ArrayList to store texts from all files
+        list = m.readFiles(m.listFiles());//assign returned ArrayList from readFile() method to "list" ArrayList
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));//print all text inside every file
