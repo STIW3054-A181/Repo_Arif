3fb4d22ba86c0444f5896c7ec1c1dd23a9c2ccbe
Nur Nadzifa Nidzam <ifa_707@yahoo.com>
2018-12-12 17:59:30 +0800

    Add files via upload

---
+++ testStandardDev.java
@@ -0,0 +1,14 @@
+package com.mavenproject.rtproject;
+
+
+public class testStandardDev {
+
+    public static void main(String[] args){
+
+        StandardDeviation s = new StandardDeviation();
+        double[] input = {9, 2, 5, 4, 12};
+        
+        System.out.println("Standard deviation is : "+s.calculateSD(input));
+
+    }
+}
