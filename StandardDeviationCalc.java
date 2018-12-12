47d13cdfc829d67a5aac00bef0fa576320f938e0
Nur Nadzifa Nidzam <ifa_707@yahoo.com>
2018-12-12 11:42:40 +0800

    Update StandardDeviationCalc.java

--- StandardDeviationCalc.java
+++ StandardDeviationCalc.java
@@ -1,37 +1,44 @@
-import java.io.IOException;
+package com.mavenproject.rtproject;
 
-public class StandardDeviationCalc {
+/*
+This class contain method to calculate standard deviation of a data that stored in array as double
+it will print average, total number in the array and the standard deviation
+the method will return the standard deviation value for other use if needed
+*/
+public class StandardDeviation {
 
-    public static void main(String[] args) throws IOException {
-        double [] values = {9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10,
-                9, 6, 9, 4 };   //change input values here
-        double sum=0;
+    public double calculate(double[] values) {
+
+        double sum = 0;
         double finalsum = 0;
         double average = 0;
 
-        for( double i : values) {
+        for (double i : values) {
             finalsum = (sum += i);
         }
 
-        average = finalsum/(values.length);
-        System.out.println("Average: "+ average);
+        average = finalsum / (values.length);
+        System.out.println("Average : " + average);
+        
+        System.out.println("Total numbers : " + values.length);
 
-        double sumX=0;
-        double finalsumX=0;
+        double sumX = 0;
+        double finalsumX = 0;
         double[] x1_average = new double[2000];
-        for (int i = 0; i<values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             double fvalue = (Math.pow((values[i] - average), 2));
-            x1_average[i]= fvalue;
-            System.out.println("test: "+ fvalue);
+            x1_average[i] = fvalue;
+            //System.out.println("test : " + fvalue);
         }
 
-        for(double i : x1_average) {
+        for (double i : x1_average) {
             finalsumX = (sumX += i);
         }
 
-        Double AverageX = finalsumX/(values.length);
-        System.out.println("E(X1-x1_average)^2/AverageX: "+ AverageX);
-        double SquareRoot = Math.sqrt(AverageX);
-        System.out.println("Standard Deviation: "+ SquareRoot);
+        Double AverageX = finalsumX / (values.length);
+        double stDeviation = Math.sqrt(AverageX);
+        System.out.println("Standard Deviation : " + stDeviation);
+
+        return stDeviation;
     }
 }
