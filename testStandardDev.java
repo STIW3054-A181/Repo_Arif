615afe76723f8a836fbd6360f1822b8053633cf7
Nur Nadzifa Nidzam <ifa_707@yahoo.com>
2018-12-12 19:59:33 +0800

    Update testStandardDev.java

--- testStandardDev.java
+++ testStandardDev.java
@@ -1,11 +1,12 @@
 package com.mavenproject.rtproject;
 
+
 public class testStandardDev {
 
     public static void main(String[] args){
 
         StandardDeviation s = new StandardDeviation();
-        double[] input = {9, 2, 5, 4, 12};
+        int [] input = {1, 2, 3, 7, 4};//different input size is allowed
         
         System.out.println("Standard deviation is : "+s.calculateSD(input));
 
