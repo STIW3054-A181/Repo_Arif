66759caa6b6bdb109c6bfabf24814fd5a1607812
AiniArasad <33992233+AiniArasad@users.noreply.github.com>
2018-12-15 19:00:29 +0800

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) throws IOException, InvalidFormatException {
         String input = "Put your string here as an input for countWord method";
 
-        System.out.println("Number Of Words\t\t: " + WordCounter.countWord(input));
+        System.out.println("Number Of Words : " + WordCounter.countWord(input));
     }
 }
