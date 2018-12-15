763a874cc99d03f7703bd1046a3de9bc101b81a3
AiniArasad <33992233+AiniArasad@users.noreply.github.com>
2018-12-15 18:59:10 +0800

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,23 +1,8 @@
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
+        String input = "Put your string here as an input for countWord method";
 
-        long startTime = System.nanoTime();
-        long start2 = System.nanoTime();
-
-       /* System.out.println("Convert .xlsx to .txt \n");
-        readwrite r = new readwrite();
-        r.readData();*/
-
-        System.out.println("Read test.txt file ....\n");
-        System.out.println("Calculate and analyse data......\n");
-        System.out.println("Open writer to Result.md file...\n");
-
-        CountingWord cw = new CountingWord();
-        cw.countWord();
+        System.out.println("Number Of Words\t\t: " + WordCounter.countWord(input));
     }
 }
