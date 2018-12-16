2bf06b7c422ac7fdbbe6b15249f9c0260834426c
Gifhary Syidhqa Hamim <43630682+gifhary@users.noreply.github.com>
2018-12-17 00:43:04 +0800

    Add files via upload

---
+++ RTProject/src/main/java/com/mavenproject/rtproject/TestWithThread.java
@@ -0,0 +1,67 @@
+/*
+This class is used to show example of how to use my methods
+ */
+package com.mavenproject.rtproject;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gifhary
+ */
+class TheThread extends Thread {
+
+    private final String textInFile;
+
+    public TheThread(String textInFile) {
+        this.textInFile = textInFile;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Number Of Words\t\t: " + WordCounter.countWord(textInFile));
+        System.out.println("Number Of Characters\t: " + CharCounter.countChar(textInFile));
+        CharCounter.charAnalysis(textInFile);
+    }
+}
+
+
+public class TestWithThread {
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        URL url = new URL("https://github.com/gifhary/Test_File/archive/master.zip");//github repo download link
+
+        File setFileName = new File("TargetFile.zip");//set the file downloaded file name
+        String zipFile = "TargetFile.zip";//input file name in String, it should be same as setFileName
+
+        AccessFiles m = new AccessFiles();//declare accessFile object
+        m.downloadZip(url, setFileName);//downloading file from URL and the file name
+        m.unzip(zipFile);//extract zip file, it cal also extract any other zip file
+
+        ArrayList<String> textInFile = new ArrayList();//ArrayList to store texts from all files
+        textInFile = m.readFiles(m.listFiles());//assign returned ArrayList from readFile() method to "list" ArrayList
+
+        ArrayList<int[]> characterNumber = new ArrayList();
+        
+        TheThread[] TT = new TheThread[textInFile.size()];//thread array as file number as the size
+
+        for (int i = 0; i < textInFile.size(); i++) {
+            //System.out.println(textInFile.get(i)); print all text inside every file
+            characterNumber.add(StandardDeviation.countCharPerWord(textInFile.get(i)));
+
+            System.out.println("");
+            System.out.println("File Name\t\t: " + m.listFiles().get(i));
+            System.out.println("Standard Deviation\t: " + StandardDeviation.calculateSD(characterNumber.get(i)));
+            
+            TT[i] = new TheThread(textInFile.get(i));//multithreading
+            TT[i].start(); //the number of the thread started depend on how many the target files is
+            
+            Thread.sleep(1000);
+        }
+
+    }
+}
